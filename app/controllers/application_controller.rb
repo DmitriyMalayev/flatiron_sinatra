0db4af0a15734e1494538bdbf060cb60528baf39
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-09-07 17:24:11 -0400

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    erb :welcome
+  end
+end
+
+
+
+
+
+# `corneal scaffold Application applicant_name:string description:text student_id:integer`   
